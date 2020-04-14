48832df1f2d645e049dce0c4c6fb5d8d9a591f04
nryan100 <nicholasryan100@gmail.com>
2020-04-14 16:43:51 -0600

    Modulated FeedParser and added more functionality

--- pyrss/model/FeedParser.py
+++ pyrss/model/FeedParser.py
@@ -7,6 +7,12 @@ class FeedParser():
         self.feed_file = feed_file
         self.articles = []
 
+    """
+        Make a request and parse with beautifulsoup
+    """
+    def makeSoup(self,url):
+        r = requests.get(str(url))
+        return BeautifulSoup(r.text,"xml")
 
     """
         Parses an xml page and extracts the following: 
@@ -16,37 +22,27 @@ class FeedParser():
         - Description
     """
     def parse(self,url):
-        if self.validate(url):
-            try: 
-                soup = BeautifulSoup(requests.get(str(url)).text, 'xml')
-
-                for item in soup.find_all("item"):                
-                    article = {
-                        "title" : item.find_all("title")[0].get_text(),
-                        "link"  : item.find_all("link")[0].get_text(),
-                        "date"  : item.find_all("pubDate")[0].get_text(),
-                        "desc"  : item.find_all("description")[0].get_text()
-                    }
-                    self.articles.append(article)
-                    #TODO REMOVE THIS LATER
-                    print(article.values())
-                    
-            except: 
-                #TODO ERROR HANDELING
-                pass
-        #TODO return empty / partial dict if invalid or unable to be read
+        try: 
+            for item in self.makeSoup(url).find_all("item"):                
+                article = {
+                    "title" : item.find_all("title")[0].get_text(),
+                    "link"  : item.find_all("link")[0].get_text(),
+                    "date"  : item.find_all("pubDate")[0].get_text(),
+                    "desc"  : item.find_all("description")[0].get_text()
+                }
+                self.articles.append(article)
+        except: 
+            # TODO: FIX THIS!
+            pass
+            
+        if not self.articles:
+            self.articles.append({
+                    "title":"URL: {} was not found".format(url),"link":"","date":"","desc": ""
+                })
 
 
     """
-        TODO: check for .xml in url
-    """
-    @staticmethod
-    def validate(url):
-        return True
-
-
-    """
-        
+        Parses a specific URL 
     """
     @staticmethod 
     def parseURL(url):
@@ -54,5 +50,12 @@ class FeedParser():
         fp.parse(url)
         return fp.articles
 
-for item in FeedParser.parseURL("https://rss.nytimes.com/services/xml/rss/nyt/Music.xml"):
-    print(item.values())
+
+    """
+        Parses all feeds defined in a text file 
+    """
+    @staticmethod
+    def parseFeedFile(file):
+        pass
+
+
