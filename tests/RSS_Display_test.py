515ce69a1d0dfd3cb179b7c75aca424759f53e76
Miles' PC <msmit396@msudenver.edu>
2020-04-19 18:27:27 -0600

    New test for the RSS Display

--- tests/RSS_Display_test.py
+++ tests/RSS_Display_test.py
@@ -5,11 +5,14 @@ class RSS_Display_test(unittest.TestCase):
 
     def test_case(self):
         self.assertEqual(1,1)
+    
+    def test_cutText(self):
+        self.assertEqual()
 
+    def test_interateArticles(self):
+        self
 
-
+if __name__ == '__main__':
+    unittest.main()
     
 
-
- 
- 
